52a29c35d66eadacf788d8bf86b55a7e97bd720f
Sheza Shamsher <160360815+ShezaShamsher@users.noreply.github.com>
2024-03-13 10:47:49 -0700

    Add files via upload

---
+++ operators.ts
@@ -0,0 +1,27 @@
+
+
+
+
+
+// There are 3 types of logical operators
+// and operator
+let num_1 : number = 8 ;
+let num_2 : number = 9 ;
+console.log( (num_1 < num_2) && (6 < 7) );
+
+console.log( (num_1 < num_2) && (7 > 9) );
+
+
+// or operator
+let num_3 : number = 4 ;
+let num_4 : number = 4 ;
+console.log( (num_3 == num_4) || (4 == 4) );
+
+
+console.log( (num_3 != num_4) || (8 > 7) );
+
+// nor operator if your answer is true this operator opposite into false
+
+let num_9 : number = 12 ;
+let num_10 : number = 10 ;
+console.log( !(num_9 > num_10) );
